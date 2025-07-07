a5a3c23cc7e82cf0cfabe9307d3748ba133f504e
Johnathon Gasbeck <jhgasbeck@gmail.com>
2025-07-06 18:03:23 -0700

    Create Zentra_analysis.sql

---
+++ SQL/Zentra_analysis.sql
@@ -0,0 +1,3 @@
+## Zentra Electronics: Additional Stakeholder Questions - Advanced SQL Queries
+
+## 1) What were the order counts, sales, and AOV for Macbooks sold in North America for each quarter across all years?
